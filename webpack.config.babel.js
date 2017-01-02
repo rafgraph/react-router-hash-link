863b95c645a3ed9e1157c294e8c458256b634350
rafrex <code@rafrex.com>
2017-01-02 13:40:00 -0800

    Install webpack dev server

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,35 +1,31 @@
 import webpack from 'webpack';
 
 export default {
-  context: __dirname,
-  entry: './index.js',
+  entry: `${__dirname}/index.js`,
   output: {
-    path: `${__dirname}/__build__`,
+    path: `${__dirname}/build`,
+    publicPath: '/build/',
     filename: 'bundle.js',
   },
+
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
     ],
   },
+
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
-  plugins: (() => {
-    if (process.argv.indexOf('-p') !== -1) {
-      return [
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: JSON.stringify('production'),
-          },
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          output: {
-            comments: false,
-          },
-        }),
-      ];
-    }
-    return [];
-  })(),
+
+  plugins: process.argv.indexOf('-p') === -1 ? null : [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false,
+      },
+    }),
+  ],
 };
