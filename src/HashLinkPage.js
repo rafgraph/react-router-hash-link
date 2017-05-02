9ac47ea7fb1e70cf7daa3d084baf91a1a6ed2cd5
rafrex <code@rafrex.com>
2017-05-02 12:15:02 -0700

    Update react and use separate prop-types package

--- src/HashLinkPage.js
+++ src/HashLinkPage.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Interactive from 'react-interactive';
 import { HashLink as Link } from 'react-router-hash-link';
 import objectAssign from 'object-assign';
