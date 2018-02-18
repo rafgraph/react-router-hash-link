a7f44eb91822a6b3e401fdfcebc00442e4d34bc2
rafrex <code@rafrex.com>
2018-02-17 16:09:52 -0800

    Add smooth scroll polyfill

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import smoothScrollPolyfill from 'smoothscroll-polyfill';
 
 import App from './App';
+
+smoothScrollPolyfill.polyfill();
 
 render(
   <BrowserRouter>
